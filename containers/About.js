d7293cad3ce24ddfa4b5e035a3f4e622d2196764
developer <1@benj.vip>
2023-05-06 01:29:43 +0300

    Update About.js

--- containers/About.js
+++ containers/About.js
@@ -9,7 +9,7 @@ const rubik = Rubik({ weight: "300", subsets: ["latin"] });
 export default function About({ id }) {
   return (
     <div className=" pb-12 px-12  flex text-xl  justify-center  items-center w-full h-full  ">
-      <div className="w-full h-full lg:flex items-center justify-center ">
+      <div className="w-full h-full lg:flex items-center md:pr-16 justify-center ">
         <div className="w-1/2' flex justify-center ">
           <Image
             src={hat}
@@ -18,13 +18,13 @@ export default function About({ id }) {
         </div>
         <div id={id} className={righteous.className}>
             <div className="w-full items-center justify-center">
-          <div className="order-0">
-            <div className="text-2xl md:text-4xl w-full lg:text-[80px] text-white px-8 ">
+          
+            <div className="text-2xl md:text-4xl w-full lg:text-[80px] text-white  ">
               about
             </div>
-          </div>
+          
           <div className={rubik.className}>
-            <div className="w-full  text-lg md:text-2xl lg:text-md text-white pt-8 px-8 ">
+            <div className="w-full  text-lg md:text-2xl lg:text-md text-white pt-8">
               Benjamin is tired of watching everyone play hot potato with the
               endless derivative ShibaCumGMElonKishuTurboAssFlokiMoon Inu coins.
               The Inu’s have had their day.It’s time for the most recognizable
